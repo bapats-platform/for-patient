68d5f15a28ebfa2cbeb657044642ffd87a99341f
aooXu <yichao.xu.19@ucl.ac.uk>
2020-10-03 02:09:25 +0800

    implement all requirements

--- pages/advice/advice.js
+++ pages/advice/advice.js
@@ -1,66 +1,37 @@
 // pages/new/advice.js
+const app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        messages: [
+            {type: "doctor", txt: "请您描述当前您的症状或者问题, 我们的系统将基于ML模型和之前的药物记录为您推荐能够缓解症状的药物. "}
+        ], 
+        paths: {}, 
+        input: ""
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.setData({
+            paths: {
+                doctor: "/assets/doctor.png", 
+                patient: app.globalData.userInfo.avatarUrl
+            }
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    onInputChange: function(e){
+        this.setData({input: e.detail.value});
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    onTapSend: function(){
+        let new_msg = this.data.messages.slice(0);
+        new_msg.push({type: "patient", txt: this.data.input});
+        this.setData({messages: new_msg, input: ""});
+        new_msg.push({type: "doctor", txt: "打一顿就好了"});
+        this.setData({messages: new_msg});
     }
 })
