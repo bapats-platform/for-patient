77fd6261070a8ee0339e5bbd63b159045219c028
aoo' Mac Mini <aoo@aoos-Mac-mini.local>
2020-10-28 22:18:18 +0800

    new about the prescription

--- app.js
+++ app.js
@@ -33,15 +33,6 @@ App({
     })
   },
   "globalData": {
-    "userInfo": null,
-    "medicineRecords": [
-      {name: "阿莫西林", type: "pill", count: "1000mg"},
-      {name: "格列本脲", type: "tablet", count: "300mg"}, 
-      {name: "头孢唑林", type: "syringe", count: "2000mg"}, 
-      {name: "苯丙醇", type: "syringe", count: "2400mg"}, 
-      {name: "美西律", type: "tablet", count: "3420mg"}, 
-      {name: "甲巯咪唑", type: "powder",  count: "400mg"}, 
-      {name: "诺氟沙星", type: "tablet", count: "2130mg"}
-    ]
+    "userInfo": null
   },
 })
--- pages/prescription/main.js
+++ pages/prescription/main.js
@@ -1,66 +1,57 @@
-// pages/prescription.js
+const app = getApp();
+
 Page({
-
-  /**
-   * Page initial data
-   */
-  data: {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page load
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+    data: {
+        user:{}, 
+        records:[],
+    },
+    onLoad: function () {
+        this.setData({
+            user: app.globalData.userInfo, 
+            records: [
+                {
+                    hospital: "养和医院", 
+                    date: "2020-09-21", 
+                    prescription: "可考虑应用可待因，其能直接抑制延脑的咳嗽中枢，镇咳作用强大而迅速，其强度约为吗啡的1/4。每日3片, 持续20天"
+                },
+                {
+                    hospital: "仁安医院", 
+                    date: "2020-09-30", 
+                    prescription: "可及时应用司坦类黏液调节剂：如羧甲司坦或祛痰剂如氨溴索，以降低痰液黏度，使痰液易于排出。每日3片, 持续20天"
+                },
+                {
+                    hospital: "港安医院", 
+                    date: "2020-10-6", 
+                    prescription: "服用抗菌药物（抗生素、磺胺药、氟喹诺酮类），消除炎症；或采取对抗过敏原（抗组胺药、‘肾上腺皮质激素）每日3片, 持续20天"
+                },
+                {
+                    hospital: "浸信会医院", 
+                    date: "2020-10-15", 
+                    prescription: "服用抗菌药物（抗生素、磺胺药、氟喹诺酮类），消除炎症；或采取对抗过敏原（抗组胺药、‘肾上腺皮质激素）每日3片, 持续20天"
+                },
+                {
+                    hospital: "圣保禄医院", 
+                    date: "2020-10-19", 
+                    prescription: "可及时应用司坦类黏液调节剂：如羧甲司坦或祛痰剂如氨溴索，以降低痰液黏度，使痰液易于排出。每日3片, 持续20天"
+                },
+                {
+                    hospital: "明德医院", 
+                    date: "2020-10-21", 
+                    prescription: "可考虑应用可待因，其能直接抑制延脑的咳嗽中枢，镇咳作用强大而迅速，其强度约为吗啡的1/4。每日3片, 持续20天"
+                },
+                {
+                    hospital: "嘉诺撒医院", 
+                    date: "2020-10-25", 
+                    prescription: "可及时应用司坦类黏液调节剂：如羧甲司坦或祛痰剂如氨溴索，以降低痰液黏度，使痰液易于排出。每日3片, 持续20天"
+                }
+              ]
+        });
+    }, 
+    onTapPrescription(event){
+        let record = this.data.records[event.currentTarget.dataset.index]
+        wx.showModal({
+            title: record.hospital,
+            content: record.prescription
+          })
+    }
+});
